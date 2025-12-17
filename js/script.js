1b5a9d83042958c8ef7682a442c6084770d0b030
triantyhandayani-cmyk <triantyhandayani@gmail.com>
2025-12-17 13:14:49 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function welcome() {
+    alert("Selamat datang di website portfolio saya!");
+}
+
+function sendMessage(event) {
+    event.preventDefault();
+    alert("Pesan berhasil dikirim!");
+}
